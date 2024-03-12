84364e9bf3df997c5d00a713020eea7267e4ff3b
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-03-12 14:07:53 +0100

    update_TodosRoute+jest.setup; create_TestDataTodos+Homework12.03.2024

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -2,23 +2,72 @@ const request = require("supertest");
 const app = require("../../../src/server");
 
 describe("GET /v1/todos/all", () => {
-  test("responds with json", async () => {
+  test("Test /all todos route", async () => {
     const response = await request(app)
       .get("/v1/todos/all")
       .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body).toEqual([]);
+    const myTodos = response.body;
+    const myFirstTodo = myTodos[0];
+
+    expect(myTodos.length).toBeGreaterThan(0);
+    expect(myFirstTodo.id).toBeDefined();
+    expect(myFirstTodo.task).toBeDefined();
+    expect(myFirstTodo.userId).toBeDefined();
   });
 
-  test("responds with json", async () => {
+  test("GET by Id", async () => {
+    const todoId = 1;
     const response = await request(app)
-      .get("/v1/todos/byid?todoId=1")
+      .get(`/v1/todos/byid?todoId=${todoId}`)
       .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body).toEqual({
-      todo: null,
-    });
+    const myTodo = response.body.todo;
+
+    expect(myTodo.id).toEqual(todoId);
+  });
+});
+
+describe("Test Mutations (PUT,POST, DELETE)", () => {
+  test("Test Create Object", async () => {
+    const response = await request(app)
+      .post(`/v1/todos/create`)
+      .send({
+        newTask: "Tennis spielen",
+        newIsDone: false,
+        newDueDate: "2026-10-10",
+        newUserId: 2,
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    const newTodo = response.body;
+
+    expect(newTodo.todo.userId).toEqual(2);
+    expect(newTodo.todo.task).toBeDefined();
+    expect(newTodo.todo.isDone).toBeDefined();
+    expect(newTodo.todo.dueDate).toBeDefined();
+  });
+
+  test("Test Put Object", async () => {
+    const response = await request(app)
+      .put(`/v1/todos/update`)
+      .send({
+        todoId: 5,
+        newTask: "Tennis spielen",
+        newIsDone: true,
+        newDueDate: "2023-10-10",
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    const updatedTodo = response.body;
+
+    expect(updatedTodo.updatedTodoId).toEqual(5);
+    // expect(updatedTodo.todo.task).toBeDefined();
+    // expect(updatedTodo.todo.isDone).toBeDefined();
+    // expect(updatedTodo.todo.dueDate).toBeDefined();
   });
 });
